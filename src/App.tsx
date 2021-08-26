6f682ca3459673d7ad9f5684749701307d47d93b
Alazar G <alazar4018@gmail.com>
2021-08-26 14:06:53 +0300

    some notes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 const App: React.FC = () => {
   return (
@@ -18,10 +19,30 @@ const Component: React.FC = () => {
     }, 3000);
   }
 
+  // use deps to avoid rerunning effects after every render.
+  useEffect(() => {
+    // this causes a run away. why?
+    // and yes we can setState from inside an effect.
+    // we just have to be carefull that it does not create a run away code.
+    setTimeout(() => setCount(Math.pow(count, 2)), 3000);
+  });
+
+  function handleClick() {
+    setCount(count + 1);
+    // react does not interupt the function here and do a render.
+    // what it does is what is wait for the event handler to finish.
+    setCount(count + 4); // meaning this call is executed.
+
+    // if react encounters multiple state setters in the course of
+    // a single event handler it batches them and chooses the latest.
+    // meaning setCount(count + 4) is called in this case.
+    console.log("run after setCount(count + 4)"); // this function gets called, too.
+  }
+
   return (
     <React.Fragment>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click Me!</button>
+      <button onClick={handleClick}>Click Me!</button>
       <button onClick={handleAlertClick}>Alert Me!</button>
     </React.Fragment>
   );
